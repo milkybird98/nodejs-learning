af9ad652fac75f499954e7431a0004ec2dab2769
milkybird98 <dengtimmycn@gmail.com>
2018-07-21 14:22:52 +0800

    lesson 3

---
+++ node-lesson-3/app.js
@@ -0,0 +1,30 @@
+var express = require("express");
+var superagent = require("superagent");
+var cheerio = require("cheerio");
+var app = express();
+
+app.get("/",function (req,res,next){
+  superagent.get('https://cnodejs.org/')
+    .end(function (err,sres) {
+      if (err) {
+        return next(err);
+      }    
+
+      let $ = cheerio.load(sres.text);
+      let item = [];
+
+      $('#topic_list .topic_title').each(function (id,element) {
+        let $element = $(element);
+        item.push({
+          title: $element.attr('title'),
+          href: $element.attr('href')
+        })
+      })
+      
+      res.send(item);
+    })
+})
+
+app.listen(3000,function () {
+  
+})
